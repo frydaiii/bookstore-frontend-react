c0f9facac1f9629558197ba1ab71ebe872726925
manh <manhsuper171@gmail.com>
2021-08-21 15:59:57 +0700

    2

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,3 +1,3 @@
-export const BACKEND_API_GATEWAY_URL = 'http://k8s-default-bookstor-874d1a00cc-1c62ffcfd0aed85c.elb.ap-east-1.amazonaws.com:8765/';
+export const BACKEND_API_GATEWAY_URL = 'http://k8s-default-bookstor-874d1a00cc-1c62ffcfd0aed85c.elb.ap-east-1.amazonaws.com:8765';
 export const APP_CLIENT_ID = '93ed453e-b7ac-4192-a6d4-c45fae0d99ac';
 export const APP_CLIENT_SECRET = 'client.devd123';
